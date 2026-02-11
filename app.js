6282d61df7524b5318d4ce65442be7ce9050cad4
TALHA-089 <rajag47786@gmail.com>
2026-02-12 03:02:58 +0500

    Checking for Failure!

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ console.log(greet("Talha"));
 console.log("Checking the pipeline...");
 
 function add(a, b){
-    return a + b;
+    return a * b;
 }
 
 module.exports = add;
--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 const add = require('./app.js');
 
-console.log("Starting Unit Test: Checking if 2 + 2 = 4...");
+console.log("Starting Unit Test: Checking if 2 + 6 = 8...");
 
-if (add(2, 2) === 4) {
+if (add(2, 6) === 8) {
   console.log("Test Passed!");
   process.exit(0);
 } else {
